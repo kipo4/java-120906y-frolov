8217c6093de4ae03844acc4f2dae09933bf717a3
kipo <kipo.vip@mail.ru>
2022-12-04 16:42:59 +0500

    initial commit

---
+++ src/lr2/Example1.java
@@ -0,0 +1,18 @@
+package lr2;
+
+import java.util.Scanner;
+
+public class Example1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Ведите число ");
+        int a = in.nextInt();
+
+        if(a % 3==0){
+            System.out.println("Программа выполняется");
+        } else{
+        System.out.println("Условие ошибочно");
+    }
+    }
+}
---
+++ src/lr2/Example3.java
@@ -0,0 +1,18 @@
+package lr2;
+
+import java.util.Scanner;
+
+public class Example3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Ведите число ");
+        int a = in.nextInt();
+
+        if(a % 4==0 && a >10){
+            System.out.println("Программа выполняется");
+        } else{
+            System.out.println("Условие ошибочно");
+        }
+    }
+}
